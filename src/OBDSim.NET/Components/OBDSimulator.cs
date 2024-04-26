899c7bf78dcadac31a3c166ef3e09b6e43bd07b0
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2024-04-26 21:41:34 +0100

    - added OBD PIDs

--- src/OBDSim.NET/Components/OBDSimulator.cs
+++ src/OBDSim.NET/Components/OBDSimulator.cs
@@ -49,6 +49,35 @@ public sealed class OBDSimulator : IDisposable
   /// </summary>
   public bool MalfunctionIndicatorLamp { get; set; } = false;
 
+  /// <summary>
+  /// List of supported modes
+  /// <see cref="http://en.wikipedia.org/wiki/OBD-II_PIDs#Mode_1_PID_03"/>
+  /// </summary>
+  public enum Mode
+  {
+    Unknown = 0x00,
+    CurrentData = 0x01,
+    FreezeFrameData = 0x02,
+    DiagnosticTroubleCodes = 0x03
+  }
+
+  /// <summary>
+  /// List of supported PIDs
+  /// <see cref="http://en.wikipedia.org/wiki/OBD-II_PIDs#Mode_1_PID_03"/>
+  /// </summary>
+  public enum PID
+  {
+    Unknown = 0x0,
+    MIL = 0x01,
+    DTCCount = 0x01,
+    Speed = 0x0D,
+    EngineTemperature = 0x05,
+    RPM = 0x0C,
+    ThrottlePosition = 0x11,
+    CalculatedEngineLoadValue = 0x04,
+    FuelPressure = 0x0A
+  };
+
   public OBDSimulator(string port, ILogger<OBDSimulator> logger)
   {
     _logger = logger;
