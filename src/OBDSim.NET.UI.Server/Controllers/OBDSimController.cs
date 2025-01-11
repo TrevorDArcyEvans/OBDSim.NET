dea31d8658f7ed55fdb45d47286ed493bc612aab
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2025-01-11 14:17:31 +0000

    - skeleton WebAPI project

---
+++ src/OBDSim.NET.UI.Server/Controllers/OBDSimController.cs
@@ -0,0 +1,40 @@
+namespace OBDSim.NET.UI.Server.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OBDSimController : ControllerBase
+{
+  // GET: api/<OBDSimController>
+  [HttpGet]
+  public IEnumerable<string> Get()
+  {
+    return new string[] { "value1", "value2" };
+  }
+
+  // GET api/<OBDSimController>/5
+  [HttpGet("{id}")]
+  public string Get(int id)
+  {
+    return "value";
+  }
+
+  // POST api/<OBDSimController>
+  [HttpPost]
+  public void Post([FromBody] string value)
+  {
+  }
+
+  // PUT api/<OBDSimController>/5
+  [HttpPut("{id}")]
+  public void Put(int id, [FromBody] string value)
+  {
+  }
+
+  // DELETE api/<OBDSimController>/5
+  [HttpDelete("{id}")]
+  public void Delete(int id)
+  {
+  }
+}
